a41f0c019d826f1099567e7d2190903f378734ec
TALEX1995 <tanase.alex@libero.it>
2023-08-23 16:47:52 +0200

    Added train info in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,11 @@
 </head>
 
 <body>
-
+  @foreach ($trains as $train)
+    <p>Il treno in arrivo da {{ $train->departure_station }} e diretto a {{ $train->arrival_station }},
+      @php echo $train->in_time ? 'non sta subendo ritardo' : 'sta subendo ritardo' @endphp
+    </p>
+  @endforeach
 </body>
 
 </html>
