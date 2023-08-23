61ab9533cdf2b2ce2031e7eb3e6f0f462efd6c95
TALEX1995 <tanase.alex@libero.it>
2023-08-23 17:47:23 +0200

    Added Train info in home

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
     use HasFactory;
+
+    public function getTime($time)
+    {
+        $timestamp = strtotime($time);
+
+        $newTime = date("H:i", $timestamp);
+
+        return $newTime;
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,39 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Trains</title>
+
+  {{-- Import assets --}}
+  @vite('resources/js/app.js')
 </head>
 
 <body>
-  @foreach ($trains as $train)
-    <p>Il treno in arrivo da {{ $train->departure_station }} e diretto a {{ $train->arrival_station }},
-      @php echo $train->in_time ? 'non sta subendo ritardo' : 'sta subendo ritardo' @endphp
-    </p>
-  @endforeach
+
+  <div class="container d-flex">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Numero Treno</th>
+          <th scope="col">Stazione di Partenza</th>
+          <th scope="col">Ora di Partenza</th>
+          <th scope="col">Stazione di Arrivo</th>
+          <th scope="col">Ora di Arrivo</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($trains as $train)
+          <tr>
+            <td>{{ $train->train_code }}</td>
+            <td>{{ $train->departure_station }}</td>
+            <td>{{ $train->getTime($train->departure_time) }}</td>
+            <td>{{ $train->arrival_station }}</td>
+            <td>{{ $train->getTime($train->arrival_time) }}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+  </div>
+
 </body>
 
 </html>
